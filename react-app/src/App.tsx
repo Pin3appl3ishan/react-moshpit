73be237e82e0f296af6bcb4add0eeff89537f2d2
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-06-21 21:47:52 +0545

    child props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "SanFrancisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
-
+ 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <Alert>
+        Hello <span>world</span>
+      </Alert>
     </div>
   );
 }
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
 
-const Alert = () => {
+interface Props{
+    children: ReactNode; // using ReactNode so that we can pass complex html elements inside the component in app.tsx
+}
+
+const Alert = ({children}: Props) => {
   return (
-    <div>Alert</div>
+    <div className='alert alert-primary'>{children}</div>
   )
 }
 
