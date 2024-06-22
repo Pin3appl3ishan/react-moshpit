fa20254a2819cef2b1f187e98b12970afc92b70a
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-06-22 11:22:32 +0545

    icon usage

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,13 @@
-import ListGroup from "./components/ListGroup";
-
+import { AiFillAndroid } from "react-icons/ai";
+//using icons
+// we need to import icon and use it's name as component to
 function App() {
-  let items = ["New York", "SanFrancisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
 
   return (
-    <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
-    </div>
+    <>
+    <AiFillAndroid />
+    
+    </>
   );
 }
 
