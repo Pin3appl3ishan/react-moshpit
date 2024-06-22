7afb1d299dbac4033a2268c0e85a9115c326e6f2
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-06-22 11:16:09 +0545

    button changes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,15 @@
-import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
 function App() {
- 
+  let items = ["New York", "SanFrancisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
   return (
     <div>
-      <Alert>
-        Hello <span>world</span>
-      </Alert>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
     </div>
   );
 }
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,11 +1,13 @@
-
 interface Props{
     children: ReactNode; // using ReactNode so that we can pass complex html elements inside the component in app.tsx
+    onClose: () => void;
 }
 
-const Alert = ({children}: Props) => {
+const Alert = ({children, onClose}: Props) => {
   return (
-    <div className='alert alert-primary'>{children}</div>
+    <div className='alert alert-primary alert-dismissible'>{children}
+    <button type="button" className="btn-close" onClick = {onClose} data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
   )
 }
 
