2645f50950193abc16f20c0fef3cd07715b66712
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-06-24 18:58:27 +0545

    state sharing

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,14 @@
-import SignIn from "./SignIn";
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
-
+const [cartItems, setCartItems] = useState(['Product1','Product2']);
   return (
-    <SignIn/>
+    <div>
+      <NavBar cartItemsCount={cartItems.length}/>
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
+    </div>
   );
 }
 
