694f528c94198673ea97af0d10e743174db0c237
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-06-24 19:14:20 +0545

    updat property of an object

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John"
-    },
+  const [cart, setCart] = useState({
+    discount: .1,
+    items: [
+      {id: 1, title: 'Product 1', quantity: 1},
+      {id: 2, title: 'Product 2', quantity: 1},
+    ],
   });
 
   const handlClick = () => {
-    setGame({...game, player: {...game.player, name: 'Gob'}})
+    setCart({...cart, items: cart.items.map(item => item.id === 1 ? {...item, quantity: item.quantity + 1} : item)})
   }
-  
+
   return (
 <button onClick={handlClick}>Click Me</button>
   );
