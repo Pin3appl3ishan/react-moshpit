1282769e82ff32bd2f61d19e7223c232360440b4
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-06-24 11:15:18 +0545

    signIn component add

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,9 @@
-import { useState } from "react";
-import { produce } from "immer"; 
+import SignIn from "./SignIn";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    {id:1, title: 'Bug 1', fixed: false},
-    {id:2, title: 'Bug 2', fixed: false},
-  ]);
-
-  const handleClick = () => {
-    setBugs(produce(draft => {
-      const bug = draft.find(bug => bug.id ===1);
-      if (bug) bug.fixed = true;
-    }))
-  };
 
   return (
-  <div>
-    {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
-     <button onClick={handleClick}>Click me</button>
-  </div>
+    <SignIn/>
   );
 }
 
