1848608ba2337c7755b68f67fa682d994c3bb6c9
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-06-24 19:06:21 +0545

    update state

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
 
 function App() {
-const [cartItems, setCartItems] = useState(['Product1','Product2']);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John"
+    },
+  });
+
+  const handlClick = () => {
+    setGame({...game, player: {...game.player, name: 'Gob'}})
+  }
+  
   return (
-    <div>
-      <NavBar cartItemsCount={cartItems.length}/>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
-    </div>
+<button onClick={handlClick}>Click Me</button>
   );
 }
 
