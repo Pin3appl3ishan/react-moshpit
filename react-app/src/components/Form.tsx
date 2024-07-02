02002103f2582ab87bbf21cf918d8fa837f8a595
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-07-02 19:46:34 +0545

    react hook form

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,27 +1,37 @@
-import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
+  const { register, handleSubmit } = useForm();
 
-    const [person, setPerson] = useState({name: '', age: ''});
-
-    const handleSumbit = (event: FormEvent) => {
-        event.preventDefault();
-        console.log(person);
-    }
+  const onSubmit = (data : FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSumbit}>
-        <div className="mb-3">
-            <label htmlFor="name" className='form-label'>Name</label>
-            <input value={person.name} onChange={(event) => setPerson({...person, name: event.target.value})} type="text" id='name' className='form-control'/>
-        </div>
-        <div className="mb-3">
-            <label htmlFor="age" className='form-label'>Age</label>
-            <input value={person.age} onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})} type="number" id='age' className='form-control'/>
-        </div>
-        <button className="btn btn-primary">Submit</button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register('name')}
+          type="text"
+          id="name"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register('age')}
+          type="number"
+          id="age"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">Submit</button>
     </form>
-  )
-} 
+  );
+};
 
-export default Form
+export default Form;
