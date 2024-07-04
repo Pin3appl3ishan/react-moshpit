a6c1babcc477277298efd296e196124197b05239
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-07-04 23:56:04 +0545

    update data

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -48,13 +48,24 @@ function App() {
     const newUser = { id: 0, name: 'Ishan'};
     setUsers([newUser, ...users])
 
-    axios.post('https://jsonplaceholder.typicode.com/xusers', newUser)
+    axios.post('https://jsonplaceholder.typicode.com/users', newUser)
     .then(({data: savedUser}) => setUsers([savedUser, ...users]))
     .catch(err => {
       setError(err.message);
       setUsers(originalUsers);
     });
+  }
 
+  const updateUser = (user: User) => {
+    const updatedUser = {...user, name: user.name + '!'};
+    const originalUsers = [...users];
+    setUsers(users.map(u => u.id === user.id ? updatedUser : u));
+
+    axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updatedUser)
+    .catch(err => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
   }
 
   return (
@@ -69,7 +80,10 @@ function App() {
             className="list-group-item d-flex justify-content-between"
           >
             {user.name}
+            <div>
+            <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
             <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+            </div>
           </li>
         ))}
       </ul>
