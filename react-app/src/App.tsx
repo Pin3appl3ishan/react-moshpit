9a5bb0ea8e5e73b338f644b2035ff6fc0b682e5e
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-07-04 18:26:59 +0545

    useEffect

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,38 +1,17 @@
 import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
+import ProductList from "./expense-tracker/components/ProductList";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  // todo: make this expense list dynamic as per the input of user
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
-    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-  ]);
-
-  // we can compute it directly through existing variable so useState is not used here
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  const [category, setCategory] = useState('');
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1}])}/>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
+      <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
+        <option value="">Select a category</option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category}/>
     </div>
   );
 }
